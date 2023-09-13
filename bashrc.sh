397b9bf0a15b5a3c812b590f1525c9b448ce5631
Adriano Gil <adrianogil.san@gmail.com>
2023-09-13 17:45:11 -0400

    Added command to run a specific test

--- bashrc.sh
+++ bashrc.sh
@@ -17,3 +17,14 @@ function npm-test-fz()
 	npm test -- ${target_file}
 }
 alias ntest-fz="npm-test-fz"
+
+
+function npm-test-fz-it()
+{
+	target_dir=$1
+	target_file=$(f "*test.js" ${target_dir} | default-fuzzy-finder)
+	target_test=$(grep -oP "it\(\s*\K'[^']*'|\"[^\"]*\"" ${target_file} | default-fuzzy-finder)
+	echo "Running $target_file"
+	npm test -- ${target_file} -t ${target_test}
+}
+alias ntest-fz-it="npm-test-fz-it"
