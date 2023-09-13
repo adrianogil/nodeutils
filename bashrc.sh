f587f2398dce941d21bc3ac1e1bd52cef1116819
Adriano Gil <adrianogil.san@gmail.com>
2023-09-13 13:01:22 -0400

    Added command to run a specific test file

--- bashrc.sh
+++ bashrc.sh
@@ -7,3 +7,13 @@ function node-fz()
 	node ${target_file}
 }
 alias nfz="node-fz"
+
+
+function npm-test-fz()
+{
+	target_dir=$1
+	target_file=$(f "*test.js" ${target_dir} | default-fuzzy-finder)
+	echo "Running $target_file"
+	npm test -- ${target_file}
+}
+alias ntest-fz="npm-test-fz"
