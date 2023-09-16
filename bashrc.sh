fb2eff55be745c05ab0cd44340c9f40253c46825
Adriano Gil <adrianogil.san@gmail.com>
2023-09-16 15:49:42 -0400

    Add node-summarize-project function
    - Introduced a new function `node-summarize-project` that provides a summary of a Node.js project.
    - Added an alias `nsummarize` for easy access to the new function.

--- bashrc.sh
+++ bashrc.sh
@@ -42,3 +42,29 @@ function npm-test-fz-it()
 }
 
 alias ntest-fz-it="npm-test-fz-it"
+
+function node-summarize-project() {
+    if [[ ! -f "package.json" ]]; then
+        echo "This doesn't seem to be a Node.js project (no package.json found)."
+        return 1
+    fi
+
+    # Count the number of JavaScript files (excluding node_modules) and their total lines
+    local js_files_count=$(find . -name "*.js" ! -path "./node_modules/*" | wc -l)
+    local total_js_loc=$(find . -name "*.js" ! -path "./node_modules/*" | xargs wc -l | tail -n 1 | awk '{print $1}')
+
+    # Count the number of installed packages (both dependencies and devDependencies)
+    local packages_count=$(jq -r '.dependencies, .devDependencies | keys | length' package.json | paste -sd+ - | bc)
+
+    # Count the number of test files (assuming they have 'test' in the filename, and excluding node_modules) and their total lines
+    local test_files_count=$(find . -name "*test*.js" ! -path "./node_modules/*" | wc -l)
+    local total_test_files_loc=$(find . -name "*test*.js" ! -path "./node_modules/*" | xargs wc -l | tail -n 1 | awk '{print $1}')
+
+    echo "Node.js Project Summary:"
+    echo "------------------------"
+    echo "Number of JavaScript files: $js_files_count ($total_js_loc lines)"
+    echo "Number of installed packages: $packages_count"
+    echo "Number of test files: $test_files_count ($total_test_files_loc lines)"
+}
+
+alias nsummarize="node-summarize-project"
