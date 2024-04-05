267a1f96b0fa95e7f19e2365c8a6d0d3d9d9f120
Adriano Gil <adrianogil.san@gmail.com>
2024-04-05 16:19:58 -0400

    refactor: added a node_test file

--- bashrc.sh
+++ bashrc.sh
@@ -2,7 +2,6 @@
 alias nd="node"
 alias nv="node --version"
 
-
 # Define a function to run a JavaScript file using Node.js with fuzzy file selection
 function node-fz()
 {
@@ -22,53 +21,6 @@ function node-project-find()
     find ${target_dir} -name "package.json" -not -path "*/node_modules/*"
 }
 alias nproj-find="node-project-find"
-
-function npm-test()
-{
-    # Use the correct version of Node.js for the project
-    nvm use
-
-    # Install dependencies
-    npm install
-
-    # Run the tests
-	npm test
-}
-alias ntest="npm-test"
-
-function npm-test-fz()
-{
-	target_dir=$1
-	target_file=$(f "*test.js" ${target_dir} | default-fuzzy-finder)
-	echo "Running $target_file"
-	npm test -- ${target_file}
-}
-alias ntest-fz="npm-test-fz"
-
-function node-list-jest-tests() {
-  if [[ ! -f $1 ]]; then
-    echo "File not found: $1"
-    return 1
-  fi
-
-  # Extract lines that have 'it(' and get the test descriptions
-  perl -ne 'while (/it\(\s*("[^"]*"|'\''[^'\'']*'\''|`[^`]*`)/g) { print "$1\n" }' "$1" | sed 's/^["'\'']//; s/["'\'']$//'
-}
-
-function npm-test-fz-it()
-
-{
-    target_dir=$1
-    target_file=$(f "*test.js" ${target_dir} | default-fuzzy-finder)
-    target_test=$(node-list-jest-tests ${target_file} | default-fuzzy-finder)
-
-    echo "Running test file $target_file"
-    echo "Running test $target_test"
-
-    npm test -- ${target_file} -t "${target_test}"
-}
-
-alias ntest-fz-it="npm-test-fz-it"
 
 function node-summarize-project() {
     if [[ ! -f "package.json" ]]; then
@@ -96,7 +48,6 @@ function node-summarize-project() {
 
 alias nsummarize="node-summarize-project"
 
-
 function npm-run-fz() {
     if [[ ! -f "package.json" ]]; then
         echo "No package.json found in the current directory."
@@ -115,3 +66,5 @@ function npm-run-fz() {
     fi
 }
 alias nrun="npm-run-fz"
+
+source ${NODE_UTILS_DIR}/node_test.sh
---
+++ node_test.sh
@@ -0,0 +1,47 @@
+function npm-test()
+{
+    # Use the correct version of Node.js for the project
+    nvm use
+
+    # Install dependencies
+    npm install
+
+    # Run the tests
+	npm test
+}
+alias ntest="npm-test"
+
+function npm-test-fz()
+{
+	target_dir=$1
+	target_file=$(f "*test.js" ${target_dir} | default-fuzzy-finder)
+	echo "Running $target_file"
+
+	npm test -- ${target_file}
+}
+alias ntest-fz="npm-test-fz"
+
+function node-list-jest-tests() {
+  if [[ ! -f $1 ]]; then
+    echo "File not found: $1"
+    return 1
+  fi
+
+  # Extract lines that have 'it(' and get the test descriptions
+  perl -ne 'while (/it\(\s*("[^"]*"|'\''[^'\'']*'\''|`[^`]*`)/g) { print "$1\n" }' "$1" | sed 's/^["'\'']//; s/["'\'']$//'
+}
+
+function npm-test-fz-it()
+
+{
+    target_dir=$1
+    target_file=$(f "*test.js" ${target_dir} | default-fuzzy-finder)
+    target_test=$(node-list-jest-tests ${target_file} | default-fuzzy-finder)
+
+    echo "Running test file $target_file"
+    echo "Running test $target_test"
+
+    npm test -- ${target_file} -t "${target_test}"
+}
+
+alias ntest-fz-it="npm-test-fz-it"
